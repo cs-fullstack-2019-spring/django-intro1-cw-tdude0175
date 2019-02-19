250c688b3f63e85a90d7b2741d824703d2f8ed59
tdude0175 <tdude0175@gmail.com>
2019-02-19 14:51:15 -0600

    reworked a different way

--- Intro1ClassWork/Intro1ClassWork/urls.py
+++ Intro1ClassWork/Intro1ClassWork/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('', include('artistRecord.urls')),
+    path('',include('artistRecord.urls')),
+    path('music/',include('artistRecord.urls')),
     path('admin/', admin.site.urls)
 ]
 
--- Intro1ClassWork/artistRecord/urls.py
+++ Intro1ClassWork/artistRecord/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = \
     [
-        path('',views.badRoute, name ="default"),
+
         path('music/',views.index, name = 'index'),
-        path('music/artistOne/',views.artOne, name = 'basic'),
-        path('music/artistTwo/', views.artTwo, name = 'secondary'),
-        path('music/artistThree/',views.artThree,name = 'final')
+        path('artistOne/',views.artOne, name = 'basic'),
+        path('artistTwo/', views.artTwo, name = 'secondary'),
+        path('artistThree/',views.artThree,name = 'final'),
+        path('',views.badRoute, name ="default"),
     ]
